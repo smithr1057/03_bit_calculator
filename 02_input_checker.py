70b2d7eddc721b8fbf248d2d520ca7d1a587a93b
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2022-08-29 09:36:19 +1200

    Add files via upload

--- 02_input_checker.py
+++ 02_input_checker.py
@@ -1,6 +1,19 @@
 # Checks user coice is 'integer', 'text' or 'image'
 def user_choice():
 
-    valid = False:
+    valid = False
     while not valid:
         
+        response = input("File type (integer / text / image): ").lower()
+
+        if response == "text" or response == "t":
+            return response 
+
+        else: 
+            print("Please choose a valid file type!")
+            print()
+
+
+
+# Main routine goes here 
+data_type = user_choice()
