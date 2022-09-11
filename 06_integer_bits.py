b539299556ad1ea97315d614804716389b61d48b
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2022-09-12 09:56:27 +1200

    Add files via upload

---
+++ 06_integer_bits.py
@@ -0,0 +1,47 @@
+def num_check(question):
+    valid = False
+    while not valid:
+        
+        error = "Please enter a number that is more than zero"
+        
+        try:
+        
+            # ask user to enter a number
+            response = float(input(question))
+            
+            # checks number is more than zero
+            if response > 0:
+                return response
+            
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+                
+        except ValueError:
+            print(error)    
+
+
+
+# converts decimal to binary and state how 
+# many bits are needed to represent the original integer
+def int_bits():
+
+    # Get integer (must be >= 0)
+    var_integer = num_check("Please enter an integer: ", 0)
+
+    # Source for code below is
+    # https://stackoverflow.com/questions/699866/python-int-to-binary-string
+
+    var_binary = "{0:b}".format(var_integer)
+
+    # calculate # of bits (length of string above)
+    num_bits = len(var_binary)
+
+    # output answer with working
+    print()
+    print("{} in binary is {}".format(var_integer, var_binary))
+    print("# of bits is {}".format(num_bits))
+    print()
+
+    
