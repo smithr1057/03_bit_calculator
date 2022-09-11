6f9bccda1d0777f17e535d1f15833d01ad9822a9
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2022-09-12 10:05:44 +1200

    Add files via upload

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -80,6 +80,28 @@ def num_check(question, low):
             print()
 
 
+# calculates the # of bits for text (# of characters x 8)
+def text_bits():
+
+    print()
+    # ask user for a string...
+    text = input("Enter some text: ")
+
+    # calculate # of bits (length of string x 8)
+    var_length = len(text)
+    num_bits = 8 * var_length
+
+    # output answer with working 
+    print()
+    print("\'{}\' has {} characters ...".format(text, var_length))
+    print("# of bits is {} x 8...".format(var_length))
+    print("We need {} bits to represent {}".format(num_bits, var_length))
+    print()
+
+    return ""
+
+
+
 # Main Routine goes here
 
 # Heading
